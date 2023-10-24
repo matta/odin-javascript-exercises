b93c0216d70b6d028c12f51874ca910a7bc14a15
Matt Armstrong <matt@rfc20.org>
2023-10-24 16:57:13 -0700

    Finish tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function () {
+const convertToCelsius = function (degrees) {
+  return round((degrees - 32) * 5 / 9)
 }
 
-const convertToFahrenheit = function () {
+const convertToFahrenheit = function (degrees) {
+  return round((degrees * 9.0 / 5.0) + 32)
+}
+
+const round = function (num) {
+  return Math.round(num * 10) / 10
 }
 
 // Do not edit below this line
