a88adf4cb8700b2394717a550618186753190bfc
Matt Armstrong <matt@rfc20.org>
2023-10-24 16:23:02 -0700

    Finish sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (min, max) {
+    if (typeof min !== 'number' || typeof max !== 'number'
+        || min < 0 || max < 0) {
+        return 'ERROR';
+    }
 
+    if (min > max) {
+        [min, max] = [max, min];
+    }
+
+    let sum = 0;
+    for (var i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
