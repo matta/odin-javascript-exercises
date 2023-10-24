f628eab9f9a66bbb512a996cda656496d1f4740c
Matt Armstrong <matt@rfc20.org>
2023-10-24 16:09:52 -0700

    Finish removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...excludedItems) {
+    return array.filter(item => !excludedItems.some(excludedItem => item === excludedItem));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
