c55e4cecb193e9ebde4da7a314b0db4795f7f99e
Matt Armstrong <matt@rfc20.org>
2023-10-26 16:40:42 -0700

    Finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const identity = x => x;
 
+const arrayEqual = function (a, b) {
+    return a.every((e, i) => e === b[i]);
+}
+
+const isAlphabetic = function (c) {
+    return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+}
+
+const isPalindrome = function (s) {
+    const l = s.toLowerCase().split('').filter(isAlphabetic);
+    return arrayEqual(l, l.map(identity).reverse());
+}
+
+const palindromes = function (s) {
+    return isPalindrome(s);
 }
 
 // Do not edit below this line
