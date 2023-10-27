edda10d66243c7434a3c595d5839e3b402e01e6e
Matt Armstrong <matt@rfc20.org>
2023-10-26 17:36:32 -0700

    Finish findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function () {
+const age = o => {
+  if (o.yearOfDeath == null) {
+    const today = new Date();
+    const age = today.getFullYear() - o.yearOfBirth;
+    return age;
+  }
+  return o.yearOfDeath - o.yearOfBirth;
+}
 
+const findTheOldest = function(a) {
+  return a.reduce((prev, curr) => {
+    if (prev === null || age(prev) < age(curr)) {
+      return curr;
+    }
+    return prev;
+  }, null);
 }
 
 // Do not edit below this line
